f2976f8766b068af8958cf8e6752031d515b1899
root <root@ip-172-31-6-255.ec2.internal>
2020-04-12 18:10:54 +0200

    Added the definition of the plugin

---
+++ slithyweb.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name: Slithy Web
+ * Plugin URI: http://my-awesomeness-emporium.com
+ * Description: a plugin to help users to setup their WordPres
+ * Version: 0.01.1
+ * Author: William Rey
+ * Author URI: http://wrey75.wordpress.com/
+ * License: GPL2
+ *
+ */
+?>
