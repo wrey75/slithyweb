86645a53f788bb70cbb0cbc20142db7b9cbf577f
William Rey <wrey75@gmail.com>
2020-04-13 17:16:11 +0200

    Added update of options

--- monitoring.php
+++ monitoring.php
@@ -24,21 +24,30 @@ class SlithyWebMonitoring {
 	if(defined('WP_DEBUG') && WP_DEBUG){
             $this->registerLogTransmission();
 	}
-	add_filter('update_option', array($this, 'option_changed'));
+	add_filter('updated_option', array($this, 'option_changed'));
     }
 
 
     function post($api, $args){
 	$url = "{$this->rootUrl}/$api";
 	$ret = wp_remote_post($url, array('headers' => $this->apiHeaders, 'body' => $args));
-	error_log("CALLED $url => $ret");
+	if( $ret instanceof WP_Error){
+		error_log("Error when calling $url: " . $ret->get_error_message());
+	} else {
+		error_log("CALLED $url => $ret");
+	}
     }
 
-    public function option_changed($option_name, $old_value, $value) {
+    public function option_changed($option_name) {
+	$value = get_option($option_name);
 	switch($option_name){
-	case 'title':
+	case 'blogname':
+	case 'blogdescription':
+	case 'siteurl':
 		$this->post("update_option", array('name'=> $option_name, 'value'=>$value));
 		break;
+	default:
+		error_log("The update of the otion '$option_name' is not transmitted.");
 	}
     }
 
