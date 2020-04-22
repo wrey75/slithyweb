24b80d1b83359adef85e9addf001e6bbe9d13b03
William Rey <wrey75@gmail.com>
2020-04-22 18:17:47 +0200

    Added an admin page

--- admin-page.php
+++ admin-page.php
@@ -1,58 +1,77 @@
 <?php
 
+
 // If this file is called directly, abort.
 if (!defined('WPINC')) die;
+
+include dirname(__FILE__)."/helpers.php";
+
+use \Slithyweb\Helper as Helper;
 
 /**
  * This class is used when the administrator pages are requested.
  */
-class SlithyWebAdministrator {
+class SlithyWebAdministrator extends Helper {
+
+    const OPT_GROUP = 'slithyweb_plugin_options';
+    const API_SECTION = 'slithyweb_api_section';
+    const MENU_SLUG = 'slithyweb_admin_menu';
+
+
     function __construct() {
-        add_action( 'admin_menu', array($this, 'add_settings_page'));
-        add_action( 'admin_init', array($this, 'register_settings'));
+        add_action( 'admin_menu', function() {
+                add_options_page( 
+                    __('Slithy Web setting', 'slithy-web'),
+                    __('Slithy web', 'slithy-web'),
+                    'manage_options', 
+                    self::MENU_SLUG, 
+                    array($this, 'render_my_settings_page'));
+        });
+        add_action( 'admin_init', array($this, 'register_my_settings'));
     }
 
-    public function add_settings_page() {
-        add_options_page( 
-            __('Slithy Web setting', 'slithy-web'),
-            __('Slithy web', 'slithy-web'),
-            'manage_options', 
-            'slithyweb-plugin', 
-            array($this, 'render_plugin_settings_page'));
-    }
-
-    function render_plugin_settings_page() {
-        echo "<h1>" . __("Slithy Web Plugin Settings", 'slithy-web') . "</h1>";
-        echo "<h2>" . __("Slithy Web hosting", 'slithy-web') . ' (<a href="https://maisonwp.com/">maisonwp.com</a>)</h2>';
+    function render_my_settings_page() {
+        printf('<h1>%s</h1>', self::T("Slithy Web Plugin Settings"));
+        printf('<h2>%s (<a href="https://maisonwp.com/">maisonwp.com</a>)</h2>', __("Slithy Web hosting", 'slithy-web'));
         if(defined("SLITHYWEB_ID")){
-            echo "<strong>" . __("Site identifier") . ":</strong> <code>" . SLITHYWEB_ID . "</code>\n";
-            _e("(can not be changed).", 'slithy-web');
+            printf("<strong>%s:<strong> <code>%s</code> (%s)\n", self::T("Site identifier"), SLITHYWEB_ID, self::T("can not be changed"));
         } else {
-            _e("This Website is not hosted by our recommended hosting service. Never mind.", 'slithy-web');
+            self::parag("This Website is not hosted by our recommended hosting service. Never mind.");
         }
-        // echo '<form action="options.php" method="post">';
-        // settings_fields( 'slithyweb_plugin_options' );
-        // do_settings_sections( 'slithyweb_api_plugin' );
-        // echo '<input name="submit" class="button button-primary" type="submit" value="' . esc_attr_e( 'Save' ) . '" />';
-        // echo '</form>';
+        if(defined("WP_DEBUG")){
+            printf("<p>%s %s</p>", self::T("WP_DEBUG is"), WP_DEBUG ? self::T("active") : self::T("disabled (PRODUCTION)"));
+        } else {
+            printf("<p>%s</p>", self::T("WP_DEBUG is not set (means PRODUCTION mode)")); 
+        }
+        echo '<form action="options.php" method="post">';
+        settings_fields(self::OPT_GROUP);
+        do_settings_sections(self::OPT_GROUP);
+        submit_button();
+        // printf( '<input name="submit" class="button button-primary" type="submit" value="%s" />', esc_attr__( 'Save' ));
+        echo '</form>';
     }
 
     /**
      * Register the settings of the administration page
      */
-    function register_settings() {
-        register_setting( 'slithyweb_plugin_options', 'slithyweb_plugin_options', array(
+    function register_my_settings() {
+        register_setting( self::OPT_GROUP, 'slithyweb_plugin_options', array(
                 'sanitize_callback' => array($this, 'plugin_options_validate')
             )
         );
-        add_settings_section( 'api_settings', 
-            __('API Settings', 'slithy-web'),
-            array($this, 'plugin_section_text'), 
-            'slithyweb_api_plugin' );
+        add_settings_section( self::API_SECTION, 
+            Helper::T('API Settings'),
+            function() {
+                printf("<p>%s<p>\n", __('Here you can set all the options for using the API', 'slithy-web'));
+            },
+            self::OPT_GROUP );
     
-        add_settings_field( 'plugin_setting_api_key', 'API Key', array($this, 'dbi_plugin_setting_api_key'), 'slithyweb_api_plugin', 'api_settings' );
-        add_settings_field( 'plugin_setting_results_limit', 'Results Limit', array($this, 'dbi_plugin_setting_results_limit'), 'slithyweb_api_plugin', 'api_settings' );
-        add_settings_field( 'plugin_setting_start_date', 'Start Date', array($this, 'dbi_plugin_setting_start_date'), 'slithyweb_api_plugin', 'api_settings' );
+        add_settings_field( 'plugin_setting_api_key', 'Google Key', function() {
+                    $options = get_option(self::API_SECTION);
+                    echo Helper::tag("input", array("id"=>'plugin_setting_api_key', "name"=>'slithyweb_plugin_options[api_key]', "type"=>'text', "value"=>$options['api_key']));
+                    }, self::OPT_GROUP, self::API_SECTION );
+        // add_settings_field( 'plugin_setting_results_limit', 'Results Limit', array($this, 'dbi_plugin_setting_results_limit'), 'slithyweb_api_plugin', 'api_settings' );
+        // add_settings_field( 'plugin_setting_start_date', 'Start Date', array($this, 'dbi_plugin_setting_start_date'), 'slithyweb_api_plugin', 'api_settings' );
     }
 
     function plugin_options_validate($input) {
@@ -66,13 +85,11 @@ class SlithyWebAdministrator {
 
     function plugin_section_text() {
         echo "<p>";
-        _e('Here you can set all the options for using the API', 'slithy-web');
         echo "</p>";
     }
 
     function dbi_plugin_setting_api_key() {
         $options = get_option( 'slithyweb_plugin_options' );
-        echo "<input id='plugin_setting_api_key' name='slithyweb_plugin_options[api_key]' type='text' value='" . esc_attr( $options['api_key'] ) . "' />";
     }
 
     function dbi_plugin_setting_results_limit() {
---
+++ helpers.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Slithyweb;
+
+class Helper { 
+
+    //
+    // HELPERS TO BE INCLUDED IN CLASSES AND PROVIDES
+    // SOME USEFUL OPERATORS WITHOUT ANY RISK OF SOME
+    // NAME DUPLICATIONS.
+    //
+    // THIS CODE CAN BE REUSED QUITE EASILY
+    //
+
+    const DOMAIN = 'slithy-web';
+
+    /**
+     * Translate a text using the defined DOMAIN.
+     */
+    public static function T($str){
+        return __($str, self::DOMAIN); 
+    }
+
+    public static function parag($str){
+        printf("<p>%s</p>\n", __($str, self::DOMAIN)); 
+    }
+
+	/**
+	 * This function creates a HTML/XML tag based
+	 * on the name and the attributes.
+	 * 
+	 * If the name finishes with "/", the tag will
+	 * be an opening one and a closing one. Typically
+	 * the tag named "img/" will produce <img .... />
+	 * 
+	 * @param string $name
+	 * @param array $attributes 
+	 */
+	public static function tag( $name, $attributes = array() ){
+		$close = ">";
+		if( $name[ strlen($name)-1 ] == "/" ){
+			$name = substr($name, 0, -1);
+			$close = "/>";
+		}
+		$tag = "<" . $name;
+  	
+		if( count($attributes) > 0 ){
+			if( !is_array($attributes) ){
+				echo "<pre>"; debug_print_backtrace(); echo "</pre>";
+				trigger_error( "array expected but [".$attributes."] received." );
+			}
+			else {
+				foreach( $attributes as $key => $value ){
+					if( is_int($key) ){
+						// Tag without "=" like "ckecked"
+						$tag .= ' ' . $value;
+					}
+					else if( isset($value) ){
+						if( is_array($value) ){
+							// When the argument is given as an array, implode it to
+							// a single value (usefull for "class" attribute). 
+							$value = implode(' ',$value);
+						}
+						$tag .= " $key=\"".str_replace("\"", "&quot;", str_replace("&", "&amp;", $value)).'"';
+					}
+				}
+			}
+		}
+		return $tag . $close;
+	}
+
+}
